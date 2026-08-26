a90313ef15b5077bcc62cb40e39889feb291ff28
ajay sharma <ajaysharma1jp@gmail.com>
2026-08-26 14:21:45 +0530

    OS detection

--- website/src/components/downloadButton.tsx
+++ website/src/components/downloadButton.tsx
@@ -10,7 +10,19 @@ type OS = 'Windows' | 'Linux' | 'Android' | 'Unknown';
 export default function DownloadButton({owner,repo}:DownloadButtonProps): JSX.Element {
     const [os, setOs] = useState<OS>('Unknown');
     const [downloadUrl, setDownloadUrl] = useState(`https://github.com/${owner}/${repo}/releases/download`);
+    const [version, setVersion] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(true);
+    useEffect(()=>{
+       const userAgent = window.navigator.userAgent;
+       let detectOS: OS='unknown';
+       if(userAgent.indexOf('Win')!==-1) detectOS = 'Windows';
+       else if(userAgent.indexOf('Linux')!==-1) detectOS = 'Linux';
+       else if(userAgent.indexOf('Android')!==-1) detectOS = 'Android';
 
+       setOs(detectOS);
+
+
+    });
     return (
         //https://github.com/DCharVault/DCharVault/releases/download/v1.2.0-alpha/DCharVault-Windows.zip
         <a href={`https://github.com/DCharVault/DCharVault/releases/download/v1.2.0-alpha/${fileName}`}
